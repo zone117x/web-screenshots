a1ec037ece0f49d1a2a1c5a25c3e560c473d29a7
Matt <zone117x@gmail.com>
2023-08-30 13:32:52 +0000

    reduce wasm bundle size

--- build/ffmpeg.sh
+++ build/ffmpeg.sh
@@ -14,6 +14,18 @@ CONF_FLAGS=(
   --disable-runtime-cpudetect   # disable cpu detection
   --disable-autodetect          # disable env auto detect
 
+  # disable all features except for ones needed to create screenshots for supported video formats
+  --disable-everything
+  --enable-avfilter
+  --enable-protocol=file
+  --enable-demuxer=matroska,mov,avi,mpegps,mpegts,m2ts # demuxers for common video containers
+  --enable-muxer=png,image2 # muxer for png file output
+  --enable-parser=mpegvideo # parse mpeg-2 m2ts files from blu-ray discs correctly
+  --enable-filter=null,trim,scale,tonemap,colorspace,format
+  --enable-encoder=png # encoder for png output
+  --enable-decoder=h264,vc1,mpeg2video,hevc # decoders for common video formats
+  --enable-swscale # software scaler library (swscale)
+
   # assign toolchains and extra flags
   --nm=emnm
   --ar=emar
